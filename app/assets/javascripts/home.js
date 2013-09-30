f1250ae58eee198decf9e3bddabdf3b8bcaf1ef2
ShaneBurkhart <shaneburkhart@gmail.com>
2013-09-30 13:02:37 -0500

    Added content editable to body

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -9,5 +9,18 @@ Blogger.home = {
                   { opacity: 1 },
                   { queue: false, duration: 1200 }
                 );
+
+    $(".try-it").click(function(event){
+      event.preventDefault();
+      //calculate destination place
+      var dest=0;
+      if($(this.hash).offset().top > $(document).height() - $(window).height()){
+          dest = $(document).height() - $(window).height();
+      }else{
+          dest = $(this.hash).offset().top;
+      }
+      //go to destination
+      $('html,body').animate({scrollTop:dest}, 500, 'swing');
+    });
   }
 }
