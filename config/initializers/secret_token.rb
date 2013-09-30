b71fe579e8d426d034637af129510e9c5f879b62
ShaneBurkhart <shaneburkhart@gmail.com>
2013-09-29 20:35:48 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleBlogLanding::Application.config.secret_token = '3a36c16f1d123343229bda1cf073b406c0355cb122d7388f570cc8374f3d4c44730109f8b83301b6fcb1998be00a5d309d7705a47af62e48a1f3fcf6db07f1d9'
