111e0881b2f9be1b25cf6e66a5b41f538f42d235
ShaneBurkhart <shaneburkhart@gmail.com>
2013-10-01 11:09:54 -0500

    Commit to branch off of

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -12,5 +12,18 @@ Blogger.home = {
     $(".try-it").click(function(){
       $(".notification").slideDown(200).delay(3000).slideUp(200);
     });
+
+    $(".reserve").click(function(event){
+      event.preventDefault();
+      //calculate destination place
+      var dest=0;
+      if($(this.hash).offset().top > $(document).height() - $(window).height()){
+          dest = $(document).height() - $(window).height();
+      }else{
+          dest = $(this.hash).offset().top;
+      }
+      //go to destination
+      $('html,body').animate({scrollTop:dest}, 500, 'swing');
+    });
   }
 }
