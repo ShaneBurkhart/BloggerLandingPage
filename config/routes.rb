8e5b00e3d48d0b78b1b998c695b632927836cd3f
ShaneBurkhart <shaneburkhart@gmail.com>
2013-10-01 17:40:19 -0500

    Front end first name added

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 SimpleBlogLanding::Application.routes.draw do
-  authenticated :user do
-    root :to => 'home#index'
-  end
   root :to => "home#index"
-  devise_for :users
-  resources :users
+  resources :users, only: ["create"]
 end
